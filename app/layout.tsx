66d9f1b7d69a30d539474f10a3fedadd2060f33e
Yulchibaev Faroxiddin <saidforgaming99@gmail.com>
2025-10-02 09:07:42 +0500

    deploy commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
 // app/layout.tsx
-import './globals.css';
-import { AppProvider } from '@/lib/context';
+import "./globals.css";
+import { AppProvider } from "@/lib/context";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     // 'class' darkMode rejimini qo'llash
-    <html lang="uz" suppressHydrationWarning> 
+    <html lang="uz" suppressHydrationWarning>
       <body>
         {/* AppProvider butun sayt bo'ylab til, rejim va kontent holatini beradi */}
-        <AppProvider>
-          {children} 
-        </AppProvider>
+        <AppProvider>{children}</AppProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
